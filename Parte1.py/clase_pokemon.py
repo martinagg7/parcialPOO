4021b109cf306150ed5df2eb830e868de6268404
Martina Garcia <martinagg@myuax.com>
2023-03-30 19:21:51 +0200

    .

--- Parte1.py/clase_pokemon.py
+++ Parte1.py/clase_pokemon.py
@@ -108,4 +108,25 @@ pokemon2_coach_2=Pokemon(25,"Venusaur","patada",78,8,6)
 pokemon3_coach_2=Pokemon(26,"Charmeleon","codazo",88,9,7)
 #descripcion
 
-#ataques
+    #ataques aleatorios
+pokemon1_coach1.atacar(pokemon1_coach1,pokemon1_coach2)
+pokemon2_coach_2.atacar(pokemon2_coach_2,pokemon1_coach1)
+pokemon3_coach_2.atacar(pokemon3_coach_2,pokemon3_coach_2)
+
+
+    #descripcion
+print("descripcion pokemon 1 coach1")
+print(pokemon1_coach1.ID)
+print(pokemon1_coach1.Nombre)
+print(pokemon1_coach1.Tipo_arma)
+
+    #defender
+print("defensa pokemon 1 coach1 VS pokemon 1 coach2")
+print(pokemon1_coach1.defender(pokemon1_coach1,pokemon1_coach2))
+print("defensa pokemon 3 coach2 VS pokemon 1 coach1")
+print(pokemon3_coach_2.defender(pokemon3_coach_2,pokemon1_coach1))
+    #descripcion
+print(pokemon1_coach1.info())
+print(pokemon2_coach1.info())
+print(pokemon3_coach1.info())
+print(pokemon1_coach2.info())
